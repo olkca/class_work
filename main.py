42f2d2266903ee924804d652efec82ffe710e823
student <zherebniuk2017@gmail.com>
2022-10-08 10:58:01 +0300

    Ex_1

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 try:
-    num1 = int(input("Число 1: "))
-    num2 = int(input("Число 2: "))
-    sum = num1 + num2
-    sum2 = sum / 2
-    print(f"Сума чисел:{sum} Середне арифматичне:{sum2}")
+    num = int(input(""))
+    num2 = int(input(""))
+    sum = 0
+    counter = num2 - num + 1
+    for i in range(num, num2 + 1):
+        sum += i
+        avg = sum / counter
+    else:
+        print(f"{sum}\n{avg}")
 except Exception as e:
-    print(f"Error...{e}")
+    print(f"Error {e}")
